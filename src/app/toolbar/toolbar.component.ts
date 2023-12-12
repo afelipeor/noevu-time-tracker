aa74f90fd32e0386c5cd4db1bec1685731a212a5
Andre Felipe <afelipeor@gmail.com>
2023-12-12 16:31:26 -0300

    Creates toolbar

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,37 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CalendarModel } from '../models/calendar.model';
 import { CantonModel } from '../models/canton.model';
 import { CantonService } from '../services/canton.service';
 
 @Component({
-  selector: 'app-toolbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss',
+    selector: 'app-toolbar',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './toolbar.component.html',
+    styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent implements OnInit {
-  @Output() numberOfDaysToShow: EventEmitter<CalendarModel[]> =
-    new EventEmitter();
+    @Output() numberOfDaysToShow: EventEmitter<CalendarModel[]> =
+        new EventEmitter();
 
-  public cantonList: CantonModel[] = [];
-  public ammountOfDays: string[] = ['Day', 'Week', 'Month'];
+    public cantonList: CantonModel[] = [];
+    public ammountOfDays: string[] = ['Day', 'Week', 'Month'];
+    public daysToShow: string = 'Week';
+    private calendar: CalendarModel[] = [];
 
-  private calendar: CalendarModel[] = [];
+    constructor(private cantonService: CantonService) {
+        this.getCantons();
+    }
 
-  constructor(private cantonService: CantonService) {
-    this.getCantons();
-  }
+    ngOnInit() {
+        this.numberOfDaysToShow.emit(this.calendar);
+    }
 
-  ngOnInit() {
-    this.numberOfDaysToShow.emit(this.calendar);
-  }
-
-  private getCantons(): void {
-    this.cantonService.getCantons().subscribe((cantons) => {
-      cantons.results.forEach((canton) =>
-        this.cantonList.push(new CantonModel(canton))
-      );
-    });
-  }
+    private getCantons(): void {
+        this.cantonService.getCantons().subscribe((cantons) => {
+            cantons.results.forEach((canton) =>
+                this.cantonList.push(new CantonModel(canton))
+            );
+        });
+    }
 }
