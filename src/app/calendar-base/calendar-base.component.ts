5e50a0b2c37c2ace4ca4644cb3e7c250ff586e6c
Andre Felipe <afelipeor@gmail.com>
2023-12-12 13:03:54 -0300

    Add rest of files

---
+++ src/app/calendar-base/calendar-base.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-base',
+  standalone: true,
+  imports: [],
+  templateUrl: './calendar-base.component.html',
+  styleUrl: './calendar-base.component.scss',
+})
+export class CalendarBaseComponent {}
