9563be138aff4cc88ff303663cd8a5b7cf0992b9
Andre Felipe <afelipeor@gmail.com>
2023-12-18 14:38:54 -0300

    creates new component

---
+++ src/app/set-workdays-modal/set-workdays-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-workdays-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-workdays-modal.component.html',
+  styleUrl: './set-workdays-modal.component.scss'
+})
+export class SetWorkdaysModalComponent {
+
+}
