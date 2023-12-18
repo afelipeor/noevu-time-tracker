da53f06174c8602af2a62bfa22b5b4299c383e00
Andre Felipe <afelipeor@gmail.com>
2023-12-18 15:57:14 -0300

    creates modal functionality

--- src/app/set-workdays-modal/set-workdays-modal.component.ts
+++ src/app/set-workdays-modal/set-workdays-modal.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { daysOfTheWeekNames } from '../constants/days-of-the-week-names.const';
 
 @Component({
-  selector: 'app-set-workdays-modal',
-  standalone: true,
-  imports: [],
-  templateUrl: './set-workdays-modal.component.html',
-  styleUrl: './set-workdays-modal.component.scss'
+    selector: 'app-set-workdays-modal',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './set-workdays-modal.component.html',
+    styleUrl: './set-workdays-modal.component.scss',
 })
 export class SetWorkdaysModalComponent {
+    @Output() showModal: EventEmitter<boolean> = new EventEmitter();
+    public readonly daysOfTheWeekNames = daysOfTheWeekNames;
 
+    /**
+     * The function closeModal emits a boolean value of false through the showModal event.
+     */
+    public closeModal(): void {
+        this.showModal.emit(false);
+    }
 }
